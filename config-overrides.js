f44759d5fd60b0d72fcd48849d36bbe69a09e2ce
gobackk1 <k.kwbt@icloud.com>
2020-05-28 10:34:51 +0900

    runtimeではconsoleを表示させない

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,13 @@
 const path = require('path')
 module.exports = (config, env) => {
+  // ランタイムではconsole.logを落とす
+  const index = config.optimization.minimizer.findIndex(m =>
+    m.options.hasOwnProperty('terserOptions')
+  )
+  config.optimization.minimizer[
+    index
+  ].options.terserOptions.compress.drop_console = true
+  // import で @エイリアスが使えるようにする
   config.resolve.alias['@'] = path.resolve(__dirname, './src')
   return config
 }
