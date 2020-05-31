a3412bb87a2bdd8a18893840d362e36ff81ab19d
gobackk1 <k.kwbt@icloud.com>
2020-05-31 10:56:05 +0900

    リファクタ

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,6 @@ export const getCookieValue = (keyword: string): string => {
   let val = ''
   document.cookie.split(';').forEach(c => {
     const [key, value] = c.split('=')
-    console.log(key, keyword)
     if (key === keyword) {
       return (val = value)
     }
